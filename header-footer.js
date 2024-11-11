32792c971432a164fc9fb8c71946784c9b79a0d6
AntonAtom <uabboyatom@gmail.com>
2024-11-11 14:04:31 +0200

    Initial commit

---
+++ header-footer.js
@@ -0,0 +1,83 @@
+const headerCtaWrap = document.querySelector(".header-main-cta_wrap");
+
+window.addEventListener("scroll", () => {
+  // Check if the screen width is greater than 991 pixels
+  if (window.innerWidth > 991) {
+    if (window.scrollY > 100) {
+      headerCtaWrap.style.maxWidth = "100%"; // 100% opacity
+    } else {
+      headerCtaWrap.style.maxWidth = "0%"; // or set to your desired opacity
+    }
+  } else {
+    // Optionally, reset the style if the screen is smaller than 991px
+    headerCtaWrap.style.maxWidth = ""; // Resets to default or hides the element
+  }
+});
+
+// Function to handle scroll event
+const headerBgWrap = document.querySelector(".header-bg_wrap");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    headerBgWrap.style.opacity = "1"; // 100% opacity
+  } else {
+    headerBgWrap.style.opacity = "0"; // or set to your desired opacity
+  }
+});
+
+const headerBgColor = document.querySelector(".header-bg");
+const headerLogo = document.querySelector(".header-logo_link");
+const headerCta = document.querySelector(".section.for-header .btn");
+const headerLinks = document.querySelectorAll(".header-link_item");
+const headerBurgerIcon = document.querySelector(".—");
+const headerDropdownToggle = document.querySelector(".header-dropdown_toggle");
+// Function to add the class
+function addLightClass() {
+  headerLogo.classList.add("u-color-light");
+  headerCta.classList.add("cc-light");
+  headerDropdownToggle.classList.add("u-color-light");
+  headerLinks.forEach((link) => {
+    link.classList.add("u-color-light");
+  });
+  headerBgColor.classList.remove("u-bg-light");
+}
+
+// Function to remove the class
+function removeLightClass() {
+  headerLogo.classList.remove("u-color-light");
+  headerCta.classList.remove("cc-light");
+  headerDropdownToggle.classList.remove("u-color-light");
+  headerLinks.forEach((link) => {
+    link.classList.remove("u-color-light");
+  });
+  headerBgColor.classList.add("u-bg-light");
+}
+$("[light-section]").each(function () {
+  let scrollTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: $(this),
+      start: "top 24px",
+      end: "bottom 24px",
+      markers: true,
+      onEnter: addLightClass,
+      onLeave: removeLightClass,
+      onEnterBack: addLightClass,
+      onLeaveBack: removeLightClass,
+    },
+  });
+});
+
+document.addEventListener("mousemove", (event) => {
+  const mouseX = event.clientX; // Get the mouse X position
+  const windowWidth = window.innerWidth; // Get the width of the window
+
+  // Calculate the percentage of the mouse X position
+  const percentageX = mouseX / windowWidth; // This will range from 0 to 1
+
+  // Calculate the mask position based on the mouse X position
+  // Scale from -200 to 200 based on percentageX
+  const maskPositionX = percentageX * 400 - 200; // Scale to fit the desired range
+
+  // Set the new mask position
+  const footerLogo = document.querySelector(".footer-big-logo_secondary");
+  footerLogo.style.webkitMaskPosition = `${maskPositionX}% 50%`;
+});
---
+++ lottie-scrub.js
@@ -0,0 +1,168 @@
+console.clear();
+/* The encoding is super important here to enable frame-by-frame scrubbing. */
+
+// ffmpeg -i ~/Downloads/Toshiba\ video/original.mov -movflags faststart -vcodec libx264 -crf 23 -g 1 -pix_fmt yuv420p output.mp4
+// ffmpeg -i ~/Downloads/Toshiba\ video/original.mov -vf scale=960:-1 -movflags faststart -vcodec libx264 -crf 20 -g 1 -pix_fmt yuv420p output_960.mp4
+
+const video = document.querySelector('.video-background');
+let src = video.currentSrc || video.src;
+console.log(video, src);
+
+/* Make sure the video is 'activated' on iOS */
+function once(el, event, fn, opts) {
+  var onceFn = function (e) {
+    el.removeEventListener(event, onceFn);
+    fn.apply(this, arguments);
+  };
+  el.addEventListener(event, onceFn, opts);
+  return onceFn;
+}
+
+once(document.documentElement, 'touchstart', function (e) {
+  video.play();
+  video.pause();
+});
+
+/* ---------------------------------- */
+/* Scroll Control! */
+
+gsap.registerPlugin(ScrollTrigger);
+
+let tl = gsap.timeline({
+  defaults: { duration: 1 },
+  scrollTrigger: {
+    trigger: '.video-wrap',
+    start: 'top top',
+    end: 'bottom bottom',
+    scrub: true,
+  },
+});
+
+once(video, 'loadedmetadata', () => {
+  tl.fromTo(
+    video,
+    {
+      currentTime: 0,
+    },
+    {
+      currentTime: video.duration || 1,
+    }
+  );
+});
+
+/* When first coded, the Blobbing was important to ensure the browser wasn't dropping previously played segments, but it doesn't seem to be a problem now. Possibly based on memory availability? */
+setTimeout(function () {
+  if (window['fetch']) {
+    fetch(src)
+      .then(response => response.blob())
+      .then(response => {
+        var blobURL = URL.createObjectURL(response);
+
+        var t = video.currentTime;
+        once(document.documentElement, 'touchstart', function (e) {
+          video.play();
+          video.pause();
+        });
+
+        video.setAttribute('src', blobURL);
+        video.currentTime = t + 0.01;
+      });
+  }
+}, 1000);
+
+/* ---------------------------------- */
+
+/* WORKING CODE FOR SCRUBBING LOTTIE */
+const animations = document.querySelectorAll('.lottie-item');
+
+animations.forEach(item => {
+  const anim = lottie.loadAnimation({
+    container: item,
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: item.dataset.json,
+  });
+
+  anim.addEventListener('DOMLoaded', () => {
+    const frameRate = 30; // Set this to the actual frame rate of your Lottie animation.
+    const targetSeconds = 4; // Target duration in seconds
+    const targetFrame = Math.floor(targetSeconds * frameRate); // Calculate target frame
+    const frameDuration = anim.getDuration(true);
+
+    console.log('Animation duration:', frameDuration);
+    console.log('Total frames:', anim.totalFrames);
+    console.log('Target Frame:', targetFrame);
+
+    // Setup ScrollTrigger
+    ScrollTrigger.create({
+      trigger: '.trigger.first',
+      start: '35% 85%',
+      end: 'bottom 85%',
+      markers: true, // Keep this for debugging
+
+      onEnter: () => {
+        console.log('Entering trigger. Playing animation.');
+        // anim.stop(); // Stop any ongoing animations
+        // anim.goToAndStop(0, true); // Reset to the beginning
+        anim.setSpeed(1);
+        anim.setDirection(1); // Set to play forward
+        anim.play(); // Start playing the animation
+        anim.playSegments([0, targetFrame], true);
+      },
+
+      onLeave: () => {
+        console.log('Leaving trigger. Stopping at target frame.');
+        // anim.stop(); // Stop any ongoing animations
+        //   anim.goToAndPlay(targetFrame, true); // Reset to the beginning
+        anim.playSegments([targetFrame, frameDuration], true);
+        anim.setSpeed(1);
+        anim.setDirection(1); // Set to play forward
+        anim.play(); // Start playing the animation
+
+        // Use GSAP to animate opacity
+        gsap.to('.lottie-frame.inner-shadow', {
+          opacity: 1, // Change opacity to 1
+          duration: 0.2, // Duration of the animation
+          ease: 'power1.out', // Easing function
+        });
+        gsap.to('.lottie-frame.blue', {
+          opacity: 1, // Change opacity to 1
+          duration: 0.2, // Duration of the animation
+          ease: 'power1.out', // Easing function
+        });
+      },
+
+      onEnterBack: () => {
+        console.log('Entering trigger back. Resetting to target frame.');
+        // anim.stop(); // Stop any ongoing animations
+        // anim.goToAndStop(targetFrame, true); // Reset to target frame
+        anim.setDirection(-1); // Set to play backwards
+        anim.play(); // Start playing the animation backwards
+        anim.playSegments([frameDuration, targetFrame], true);
+        anim.setSpeed(4);
+        // Use GSAP to animate opacity
+        gsap.to('.lottie-frame.inner-shadow', {
+          opacity: 0, // Change opacity to 1
+          duration: 0.2, // Duration of the animation
+          ease: 'power1.out', // Easing function
+        });
+        gsap.to('.lottie-frame.blue', {
+          opacity: 0, // Change opacity to 1
+          duration: 0.2, // Duration of the animation
+          ease: 'power1.out', // Easing function
+        });
+      },
+
+      onLeaveBack: () => {
+        console.log('Leaving trigger back. Playing animation backwards.');
+        // anim.stop(); // Stop any ongoing animations
+        // anim.goToAndStop(targetFrame, true); // Reset to target frame
+        anim.setDirection(-1); // Set to play backwards
+        anim.play(); // Start playing the animation backwards
+        anim.playSegments([targetFrame, 0], true);
+        anim.setSpeed(4);
+      },
+    });
+  });
+});
