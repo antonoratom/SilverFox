930efe5b3c1f8005a35d33e9ee61b9eb3f7f5f53
AntonAtom <uabboyatom@gmail.com>
2025-01-09 22:58:41 +0200

    Update steps.js

--- steps.js
+++ steps.js
@@ -5,7 +5,7 @@ $("[steps-card-target]").each(function () {
       start: "top 30%",
       end: "300% 30%",
       scrub: true,
-      //   markers: true,
+      // markers: true,
     },
   });
 
@@ -14,4 +14,29 @@ $("[steps-card-target]").each(function () {
     // y: "100vh",
     ease: "linear",
   });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const stepsElements = document.querySelectorAll(".steps-bl");
+  const customHeightElements = document.querySelectorAll(".custom-height");
+  const totalCustomHeight = Array.from(customHeightElements).reduce(
+    (sum, element) => sum + element.clientHeight,
+    0
+  );
+
+  if (stepsElements.length >= 4) {
+    const firstElement = stepsElements[0];
+    const secondElement = stepsElements[1];
+    const thirdElement = stepsElements[2];
+    const fourthElement = stepsElements[3];
+
+    firstElement.style.marginBottom = `${totalCustomHeight * 3}px`;
+    secondElement.style.marginTop = `-${totalCustomHeight * 3}px`;
+
+    secondElement.style.marginBottom = `${totalCustomHeight * 2}px`;
+    thirdElement.style.marginTop = `-${totalCustomHeight * 2}px`;
+
+    thirdElement.style.marginBottom = `${totalCustomHeight}px`;
+    fourthElement.style.marginTop = `-${totalCustomHeight}px`;
+  }
 });
